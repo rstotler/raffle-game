1d87e08b8600550674771d196090a9a0a92e03c1
rstotler <rstotler87@gmail.com>
2024-10-31 01:40:57 -0400

    Initial Commit

---
+++ desktop/src/com/jbs/rafflegame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jbs.rafflegame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jbs.rafflegame.RaffleGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("RaffleGame");
+		new Lwjgl3Application(new RaffleGame(), config);
+	}
+}
